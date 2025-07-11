722ae0911a2bbe46066f33fa9af8e8e264e352df
Gourav Singh <gourav.oct@gmail.com>
2025-07-11 19:09:31 +0530

    config pom ang kafka/jackson

--- src/main/java/com/app/food_delivery_backend/FoodDeliveryBackendApplication.java
+++ src/main/java/com/app/food_delivery_backend/FoodDeliveryBackendApplication.java
@@ -2,8 +2,10 @@ package com.app.food_delivery_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class FoodDeliveryBackendApplication {
 
 	public static void main(String[] args) {
