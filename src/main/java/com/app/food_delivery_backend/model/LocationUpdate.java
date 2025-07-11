b532adfcc48058417aca59f82ec088dd128de732
Gourav Singh <gourav.oct@gmail.com>
2025-07-11 19:20:36 +0530

    modified LocationUpdate

--- src/main/java/com/app/food_delivery_backend/model/LocationUpdate.java
+++ src/main/java/com/app/food_delivery_backend/model/LocationUpdate.java
@@ -1,13 +1,20 @@
 package com.app.food_delivery_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocationUpdate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +23,18 @@ public class LocationUpdate {
     private String agentId;
     private double latitude;
     private double longitude;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime timestamp;
+
+    @Override
+    public String toString() {
+        return "LocationUpdate{" +
+                "id=" + id +
+                ", agentId='" + agentId + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
